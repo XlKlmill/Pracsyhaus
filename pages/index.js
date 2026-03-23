161dec7f01c72c0e145527c75e68f97dd0a43a03
XlKlmill <xlkmill@gmail.com>
2026-03-23 13:01:01 +0100

    Refactor language handling and add language selection UI
    Removed server-side language detection and redirection. Added a language selection interface.

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,15 @@
-export async function getServerSideProps({ req }) {
-  const langHeader = req.headers["accept-language"];
-
-  let lang = "en";
-
-  if (langHeader) {
-    lang = langHeader.split(",")[0].split("-")[0];
-  }
-
-  const supported = ["en", "de", "fr", "es", "it", "pl"];
-
-  const locale = supported.includes(lang) ? lang : "en";
-
-  return {
-    redirect: {
-      destination: `/${locale}`,
-      permanent: false
-    }
-  };
-}
-
 export default function Home() {
-  return null;
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Snowhitextiles</h1>
+      <p>Select your language:</p>
+
+      <a href="/en">English</a><br/>
+      <a href="/de">Deutsch</a><br/>
+      <a href="/fr">Français</a><br/>
+      <a href="/es">Español</a><br/>
+      <a href="/it">Italiano</a><br/>
+      <a href="/pl">Polski</a>
+    </div>
+  );
 }
